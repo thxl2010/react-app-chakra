72585cd66c4944d49cb0ab41bb5fbe0aeba5c1c2
Duyb <1255248666@qq.com>
2022-03-22 21:37:59 +0800

    Chakra-UI: Component Button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Stack, HStack, Button, Center } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg="#f3f3f3" w="100%" p={4} color="#333" borderRadius="lg">
+      <HStack spacing="24px">
+        <Box w="100px" h="40px" bg="yellow.200">
+          HStack Box 1
+        </Box>
+        <Box p="1em" bg="tomato">
+          HStack Box 2
+        </Box>
+        <Box h="40px" bg="pink.100">
+          HStack Box 3
+        </Box>
+      </HStack>
+      <Stack spacing={4} direction="row" align="center">
+        <Button colorScheme="blue">按钮</Button>
+        <Button colorScheme="blue" size="xs">
+          按钮 xs
+        </Button>
+        <Button colorScheme="blue" size="sm">
+          按钮 sm
+        </Button>
+        <Button colorScheme="blue" size="md">
+          按钮 md
+        </Button>
+        <Button colorScheme="blue" size="lg">
+          按钮 lg
+        </Button>
+      </Stack>
+      <Center bg="tomato" h="100px" color="white" margin="1em">
+        This is the Center
+      </Center>
+    </Box>
   );
 }
 
