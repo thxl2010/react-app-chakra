3e75dff2ebc0fd4c067319e086e51b08ea785e8a
Duyb <1255248666@qq.com>
2022-04-17 23:05:36 +0800

    part4-module3-task7-work: 将简书登录框封装成一个登录/注册组件

--- src/views/jianshu/SignIn.js
+++ src/views/jianshu/SignIn.js
@@ -1,3 +1,38 @@
+import {
+  Input,
+  Stack,
+  InputGroup,
+  InputLeftAddon,
+  FormControl,
+  FormHelperText,
+  Checkbox,
+  Button,
+} from '@chakra-ui/react';
+import { FaUserAlt, FaLock } from 'react-icons/fa';
+
 export default function SignIn() {
-  return <>login</>;
+  return (
+    <form>
+      <Stack spacing="8">
+        <FormControl isInvalid isRequired>
+          <InputGroup>
+            <InputLeftAddon children={<FaUserAlt />} />
+            <Input placeholder="手机号或邮箱" />
+          </InputGroup>
+          {/* <FormHelperText>请输入手机号或邮箱</FormHelperText> */}
+        </FormControl>
+        <InputGroup>
+          <InputLeftAddon children={<FaLock />} />
+          <Input type="password" placeholder="密码" />
+        </InputGroup>
+        <Stack direction="row" justify="space-between">
+          <Checkbox>记住我</Checkbox>
+          <a href="https://www.jianshu.com/sessions">登陆遇到问题？</a>
+        </Stack>
+        <Button colorScheme="blue" w="100%" borderRadius="20px">
+          登录
+        </Button>
+      </Stack>
+    </form>
+  );
 }
--- src/views/jianshu/SignUp.js
+++ src/views/jianshu/SignUp.js
@@ -1,3 +1,66 @@
+import {
+  Input,
+  Stack,
+  InputGroup,
+  InputLeftAddon,
+  FormControl,
+  FormHelperText,
+  RadioGroup,
+  Radio,
+  Select,
+  Switch,
+  FormLabel,
+  Flex,
+  Button,
+} from '@chakra-ui/react';
+import { FaUserAlt, FaMobileAlt, FaLock } from 'react-icons/fa';
+
 export default function SignUp() {
-  return <>register</>;
+  return (
+    <form>
+      <Stack spacing="8">
+        <FormControl isInvalid isRequired>
+          <InputGroup>
+            <InputLeftAddon children={<FaUserAlt />} />
+            <Input placeholder="你的昵称" />
+          </InputGroup>
+          <FormHelperText>请输入昵称</FormHelperText>
+        </FormControl>
+        <FormControl isInvalid isRequired>
+          <InputGroup>
+            <InputLeftAddon children={<FaMobileAlt />} />
+            <Input placeholder="手机号" />
+          </InputGroup>
+          <FormHelperText>请输入手机号</FormHelperText>
+        </FormControl>
+        <InputGroup>
+          <InputLeftAddon children={<FaLock />} />
+          <Input type="password" placeholder="设置密码" />
+        </InputGroup>
+        <Button colorScheme="green" w="100%">
+          注册
+        </Button>
+        <Stack textAlign="center" fontSize={12}>
+          <p>点击 “注册” 即表示您同意并愿意遵守简书</p>
+          <Flex justify="center">
+            <a
+              target="_blank"
+              href="http://www.jianshu.com/p/c44d171298ce"
+              rel="noreferrer"
+            >
+              用户协议
+            </a>
+            <span>&nbsp;和&nbsp;</span>
+            <a
+              target="_blank"
+              href="http://www.jianshu.com/p/2ov8x3"
+              rel="noreferrer"
+            >
+              隐私政策
+            </a>
+          </Flex>
+        </Stack>
+      </Stack>
+    </form>
+  );
 }
