6b9645aaa6c84025c1d434e734d325a5f59e216e
Duyb <1255248666@qq.com>
2022-04-19 18:53:58 +0800

    part4-module3-task7-work: 将简书登录框封装成一个登录/注册组件

--- src/views/jianshu/SignUp.js
+++ src/views/jianshu/SignUp.js
@@ -7,32 +7,119 @@ import {
   FormHelperText,
   Flex,
   Button,
+  useToast,
 } from '@chakra-ui/react';
-import { FaUserAlt, FaMobileAlt, FaLock } from 'react-icons/fa';
+import { FaUserAlt, FaEnvelope, FaLock } from 'react-icons/fa';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signUp } from '../../api/jianshu';
 
 export default function SignUp() {
+  const toast = useToast();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      console.log(JSON.stringify(values, null, 2));
+      setLoading(true);
+      try {
+        const { user } = await signUp(values);
+        const { username } = user;
+        console.log('user :', user);
+        navigate(`/jianshu/${username}`, { state: user });
+      } catch (error) {
+        console.log('error :', error);
+        toast({
+          title: '昵称或密码错误，请重试！',
+          status: 'error',
+          isClosable: true,
+          position: 'top',
+        });
+      }
+      setLoading(false);
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(
+          2,
+          '昵称格式不正确，需要是2-15个字符，只能包含英文中文下划线，不能包含空格。'
+        )
+        .max(
+          15,
+          '昵称格式不正确，需要是2-15个字符，只能包含英文中文下划线，不能包含空格。'
+        )
+        .required('请输入昵称'),
+      email: Yup.string().email('请输入合法的邮箱地址').required('请输入邮箱'),
+      password: Yup.string().min(6, '密码长度不能小于6').required('请输入密码'),
+    }),
+  });
+
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <Stack spacing="8">
         <FormControl isInvalid isRequired>
           <InputGroup>
             <InputLeftAddon children={<FaUserAlt />} />
-            <Input placeholder="你的昵称" />
+            <Input
+              placeholder="你的昵称"
+              id="signup-username"
+              type="username"
+              name="username"
+              {...formik.getFieldProps('username')}
+            />
           </InputGroup>
-          <FormHelperText>请输入昵称</FormHelperText>
-        </FormControl>
-        <FormControl isInvalid isRequired>
+          <FormHelperText>
+            {formik.touched.username && formik.errors.username
+              ? formik.errors.username
+              : null}
+          </FormHelperText>
           <InputGroup>
-            <InputLeftAddon children={<FaMobileAlt />} />
-            <Input placeholder="手机号" />
+            <InputLeftAddon children={<FaEnvelope />} />
+            <Input
+              placeholder="邮箱"
+              id="signup-email"
+              type="email"
+              name="email"
+              {...formik.getFieldProps('email')}
+            />
           </InputGroup>
-          <FormHelperText>请输入手机号</FormHelperText>
+          <FormHelperText>
+            {formik.touched.email && formik.errors.email
+              ? formik.errors.email
+              : null}
+          </FormHelperText>{' '}
+          <InputGroup>
+            <InputLeftAddon children={<FaLock />} />
+            <Input
+              type="password"
+              placeholder="设置密码"
+              id="signup-password"
+              name="password"
+              {...formik.getFieldProps('password')}
+            />
+            <FormHelperText>
+              {formik.touched.password && formik.errors.password
+                ? formik.errors.password
+                : null}
+            </FormHelperText>
+          </InputGroup>
         </FormControl>
-        <InputGroup>
-          <InputLeftAddon children={<FaLock />} />
-          <Input type="password" placeholder="设置密码" />
-        </InputGroup>
-        <Button colorScheme="green" w="100%">
+
+        <Button
+          colorScheme="green"
+          w="100%"
+          borderRadius="20px"
+          type="submit"
+          isLoading={loading}
+        >
           注册
         </Button>
         <Stack textAlign="center" fontSize={12}>
